20007a12711b07efc8a521639715b096bdef5a02
Juan Ortiz <juan.ortiz@kavak.com>
2022-07-13 10:31:27 -0500

    feat: basic component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React from "react";
+import Searchbox from "./components/searchbox/searchbox";
+import Article from "./components/article/article";
 
 const App: React.FC = () => {
   return (
-    <p>Hello World</p>
+    <>
+      <Searchbox></Searchbox>
+      <Article></Article>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/searchbox/searchbox.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Searchbox: React.FC = () => {
+  return (
+    <p>searchbox</p>
+  )
+}
+
+export default Searchbox;
